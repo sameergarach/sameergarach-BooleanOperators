485dd683e139b67dde2fc769979ea0b9f7178bc1
Sameer Garach <sameer.garach@gmail.com>
2023-03-05 19:49:03 +0000

    initial commit

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -13,8 +13,8 @@ public class Lab {
      * @param b a true/false value.
      * @return if b is true, return true. if b is false, return false.
      */
-    public boolean returnBoolean(boolean b){
-        return false;
+    public boolean returnBoolean (boolean b){
+        return b;
     }
 
     /**
@@ -23,7 +23,7 @@ public class Lab {
      * look into boolean operations to do this without using if/else statements.
      */
     public boolean returnNot(boolean b){
-        return false;
+        return !b;
     }
 
     /**
@@ -33,7 +33,8 @@ public class Lab {
      * look into boolean operations to do this without using if/else statements.
      */
     public boolean returnAnd(boolean b1, boolean b2){
-        return false;
+        return (b1 && b2);
+        
     }
 
     /**
@@ -43,7 +44,7 @@ public class Lab {
      * look into boolean operations to do this without using if/else statements.
      */
     public boolean returnOr(boolean b1, boolean b2){
-        return false;
+        return (b1 || b2);
     }
 
 
